7d1c8100e20bc1e91540b47b8b9ea350d4c5ba1f
Michelle Ho <michelle321ho@gmail.com>
2018-10-02 12:26:18 -0700

    added num, avg, count, fact args

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,34 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var size = stringArray.count
+        var numbers: [Int] = []
+        var result = 0;
+        for i in 0...size-1 {
+            let arg = stringArray[i]
+            switch arg {
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                numbers.append((arg as NSString).integerValue)
+            case "avg":
+                var sum = 0;
+                for j in 0...numbers.count-1 {
+                    sum = sum + numbers[j]
+                }
+                result = sum / numbers.count
+            case "count":
+                result = numbers.count
+            case "fact":
+                if numbers[1] == 0 { result = 1 }
+                else {
+                    for j in (1...numbers[0]) {
+                        result = result * j
+                    }
+                }
+            default:
+                print(arg)
+            }
+        }
+        return result
     }
     
     public func calculate(_ arg: String) -> Int {
